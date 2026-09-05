8658c25c26699571b815a815cab7d4eec9b0427e
Nathan5S61 <siumy26@gmail.com>
2026-09-05 14:00:50 +0800

    Four-leaf rose number

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# rose_number
-Nothing special
+# Four-leaf Rose Number
+This project is introduce rose_number
+
+## Rose number.pdf
+Pdf and tex(LaTeX code) is in release
+Inside rose number.pdf, there have some script about python and cpp that you can copy to compiler and run.
+
+## Rose number.7z
+Inside Rose number.7z has some script.
---
+++ Rose_number/Rose_number.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cstdio>
+using namespace std;
+int main()
+{
+	int a, b, c, d;
+	for (int num = 1000; num <= 9999; num++) {
+		a = num / 1000;
+		b = (num / 100) % 10;
+		c = (num / 10) % 10;
+		d = num % 10;
+		if (a * a * a * a + b * b * b * b + c * c * c * c + d * d * d * d == num)
+			printf("%d\n", num);
+	}
+	cin.get();
+	return 0;
+}
